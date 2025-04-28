96d684ee8049755f1dddae77efd21b49da738b8d
Brandyn Coverdill <brandyncoverdill98@gmail.com>
2025-04-27 21:30:00 -0600

    Update WeatherService class to improve switch-statement

--- src/main/java/com/devvesper/weather_stats/service/implementations/CityWeatherService.java
+++ src/main/java/com/devvesper/weather_stats/service/implementations/CityWeatherService.java
@@ -7,15 +7,12 @@ import org.springframework.stereotype.Service;
 public class CityWeatherService implements CityWeatherInterface {
     @Override
     public String getWeather(String city) {
-        switch (city) {
-            case "nampa":
-                return "Today's forecast - high: 67, low: 48, precipitation: 45%, humidity: 51%, wind: 10mph";
-            case "cedar-rapids":
-                return "Today's forecast - high: 68, low: 56, precipitation: 5%, humidity: 47%, wind: 12mph";
-            case "los-angeles":
-                return "Today's forecast - high: 61, low: 54, precipitation: 20%, humidity: 56%, wind: 6mph";
-            default:
-                return "There is not a city named " + city;
-        }
+        return switch (city) {
+            case "nampa" -> "Today's forecast - high: 67, low: 48, precipitation: 45%, humidity: 51%, wind: 10mph";
+            case "cedar-rapids" ->
+                    "Today's forecast - high: 68, low: 56, precipitation: 5%, humidity: 47%, wind: 12mph";
+            case "los-angeles" -> "Today's forecast - high: 61, low: 54, precipitation: 20%, humidity: 56%, wind: 6mph";
+            default -> "There is not a city named " + city;
+        };
     }
 }
